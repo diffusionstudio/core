801c44a8f71094966a8c8a6b6809f62b29764c7d
konstantin-paulus <mail@konstantinpaulus.de>
2024-09-03 12:22:08 +0200

    transferred examples to separate repo

--- examples/scripting/src/types.ts
+++
@@ -1,4 +0,0 @@
-import * as core from '@diffusionstudio/core';
-
-export type Settings = Partial<core.CompositionSettings> | undefined;
-export type MainFn = (composition: core.Composition) => Promise<void>;
