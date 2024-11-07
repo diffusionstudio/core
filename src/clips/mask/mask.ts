3df5e2a270e98ff48a6e8761d45addc45ecd19a5
Matze99 <hamachermatthias12@gmail.com>
2024-11-07 12:08:59 -0800

    removed comment

--- src/clips/mask/mask.ts
+++ src/clips/mask/mask.ts
@@ -1,9 +1,6 @@
 import { Graphics } from "pixi.js";
 import type { MaskProps } from "./mask.types";
 
-/**
- * A mask is used to mask another visual clip.
- */
 export class Mask extends Graphics {
     constructor(props: MaskProps) {
         super();
